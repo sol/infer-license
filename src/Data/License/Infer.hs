2419b08cfb7bb49270e57eadfe3585289a8bcf85
Simon Hengel <sol@typeful.net>
2018-07-15 11:13:02 +0800

    Recognize GPL, LGPL and AGPL

--- src/Data/License/Infer.hs
+++ src/Data/License/Infer.hs
@@ -5,6 +5,10 @@ module Data.License.Infer (
 , inferLicense
 ) where
 
+import           Control.Applicative
+import           Control.Monad
+import           Data.Foldable
+import           Data.Char
 import           Data.List
 import           Data.Ord (comparing)
 import           Data.Text (Text)
@@ -15,7 +19,28 @@ import           Data.License.SpdxLicenses (licenses)
 import           Data.License.Type
 
 inferLicense :: String -> Maybe License
-inferLicense (T.pack -> xs) = case maximumBy (comparing snd) (probabilities xs) of
+inferLicense xs = inferLicenseByName xs <|> inferLicenseByLevenshtein xs
+
+inferLicenseByName :: String -> Maybe License
+inferLicenseByName (normalize -> xs) = asum $ map (matchName xs) licenseNames
+
+matchName :: String -> (License, String) -> Maybe License
+matchName xs (license, name) = license <$ guard (isPrefixOf name xs)
+
+licenseNames :: [(License, String)]
+licenseNames = map (fmap normalize) [
+    (GPLv2, "GNU GENERAL PUBLIC LICENSE Version 2, June 1991")
+  , (GPLv3, "GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007")
+  , (LGPLv2_1, "GNU LESSER GENERAL PUBLIC LICENSE Version 2.1, February 1999")
+  , (LGPLv3, "GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June 2007")
+  , (AGPLv3, "GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007")
+  ]
+
+normalize :: String -> String
+normalize = map toLower . filter isAlphaNum
+
+inferLicenseByLevenshtein :: String -> Maybe License
+inferLicenseByLevenshtein (T.pack -> xs) = case maximumBy (comparing snd) (probabilities xs) of
   (license, n) | n > 0.85 -> Just license
   _ -> Nothing
 
--- src/Data/License/Type.hs
+++ src/Data/License/Type.hs
@@ -2,7 +2,16 @@ module Data.License.Type where
 
 data License =
     MIT
+
   | BSD2
   | BSD3
   | BSD4
+
+  | GPLv2
+  | GPLv3
+
+  | LGPLv2_1
+  | LGPLv3
+
+  | AGPLv3
   deriving (Eq, Show, Bounded, Enum)
